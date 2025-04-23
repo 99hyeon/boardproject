8e863e77b69ced7c9cb01ed78f63ae704167c379
99hyeon <hyeon99@ajou.ac.kr>
2025-04-23 13:31:44 +0900

    project setting

---
+++ src/main/java/com/example/boardproject/domain/post/Post.java
@@ -0,0 +1,30 @@
+package com.example.boardproject.domain.post;
+
+import com.example.boardproject.domain.comment.Comment;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String contents;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Comment> comments = new ArrayList<>();
+
+}
